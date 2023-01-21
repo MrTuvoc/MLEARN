3f555495bdc12a39d1a6bb49a814ebebb5ab3266
MrTuvoc <emin.chniti@esprit.tn>
2023-01-21 19:50:02 +0100

    ⬆️ Upgraded dependencies

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 
 A simple machine learning algorithm in python, trained to predict the most successful movie genre of a given year based on datalist of netflix movies.
 
+## Deployment
+
+To deploy this project import "mlearn.ipynb" to jupyter notebook
+
+For native deployment:
+```
+  python3 mlearn.py
+```
+
 ## Dependencies
 ```
   pandas
@@ -13,16 +22,6 @@ Simply run:
 ```
   pip install -r requirements.txt
 ```
-
-## Usage
-
-To use this project import "mlearn.ipynb" to jupyter notebook after cloning the repository
-
-Or use:
-```
-  python3 mlearn.py
-```
-
 ## Data Visualization
 
-![](graphviz.png)
+![](visualization/graphviz.png)
--- mlearn.py
+++ mlearn.py
@@ -8,21 +8,21 @@ from sklearn import tree
 
 # prediction / training function
 def predict(year):
-	movie_data = pd.read_csv('best_movies_netflix.csv')
+	movie_data = pd.read_csv('data/best_movies_netflix.csv')
 	X = movie_data.drop(columns=['title','number_of_votes','duration','main_genre','main_production'])
 	y = movie_data['main_genre']
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 	model = DecisionTreeClassifier()
 	model.fit(X_train, y_train)
     # output model
-	joblib.dump(model, 'movie-predict.joblib')
+	joblib.dump(model, 'model/movie-predict.joblib')
     # load model instead of training everytime
-    # model = joblib.load('music-predict.joblib')
+    # model = joblib.load('model/music-predict.joblib')
     # output visualization
-	tree.export_graphviz(model, out_file='movie-predict.dot', feature_names=['release_year','score'], class_names=sorted(y.unique()), label='all', rounded=True, filled=True)
+	tree.export_graphviz(model, out_file='visualization/movie-predict.dot', feature_names=['release_year','score'], class_names=sorted(y.unique()), label='all', rounded=True, filled=True)
 	pred = model.predict(X_test)
 	score = accuracy_score(y_test, pred) # AI accuracy score
-	return model.predict([[str(year),'9']])
+	return model.predict([[str(year),movie_data['score'].max()]])
 
 # input/output function
 def take():
